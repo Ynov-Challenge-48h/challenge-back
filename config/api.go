3769078fcb256739c040adf77a40c6dc3628ee88
remy.guibert@ynov.com <remy.guibert@ynov.com>
2025-04-03 14:45:36 +0200

    � WIP : Fix Temp Structure For the Api Project �

--- config/api.go
+++ config/api.go
@@ -2,14 +2,10 @@ package config
 
 import "api_test/data"
 
-// ConfigDataApi initializes and returns a DataApi instance with predefined album data.
-func ConfigDataApi() *data.DataApi {
-	return &data.DataApi{
-		// Predefined list of albums with their respective details.
-		Albums: []data.Album{
-			{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-			{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-			{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-		},
+// ConfigApi initializes and returns an ApiConfig instance with host and port settings.
+func ConfigApi() *data.ApiConfig {
+	return &data.ApiConfig{
+		Host: "localhost", // Host on which the application will run.
+		Port: "8080",      // Port on which the application will run.
 	}
 }
